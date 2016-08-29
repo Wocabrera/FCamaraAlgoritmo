5b450c1e635a930c4279fa757d4115093e336dd1
Wocabrera <wocabrera@gmail.com>
2016-08-29 18:35:44 -0300

    first commit

---
+++ ConsoleApplicationStreamRead/MyStream.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationStreamRead {
+  public class MyStream : IStream {
+
+    public MyStream(byte[] b) {
+      byteArray = b;
+      this.stream = new MemoryStream(byteArray, 0, byteArray.Length);
+    }
+
+    public byte[] byteArray { get; set; }
+    public Stream stream { get; set; }
+
+    public string u = String.Empty;
+    public char getNext() {
+      return (Char)stream.ReadByte();
+    }
+
+    public bool hasNext() {
+      if (byteArray.Length > stream.Position)
+        return true;
+      return false;
+    }
+
+  }
+}
---
+++ ConsoleApplicationStreamRead/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationStreamRead {
+
+
+  class Program {
+    static void Main(string[] args) {
+
+      Console.WriteLine("Entre com um texto");
+      var input = Console.ReadLine();
+
+      IStream iStrm = new MyStream(Encoding.UTF8.GetBytes(input));
+      var output = firstChar(iStrm);
+      Console.WriteLine($"Mensagem de saida : {output}" );
+
+      Console.ReadKey();
+
+    }
+
+    public static char firstChar(IStream input) {
+
+      var word = "";
+      var final = ' ';
+      var ini = -1;
+
+
+      while (input.hasNext()) {
+        var ret = input.getNext();
+        word += ret;
+      }
+
+      for (int i = 0; i < word.Length; i++) {
+        if (!"aeiou".Contains(word[i])) {
+          ini = i;
+          break;
+        }
+      }
+
+      if (ini >= 0)
+        for (int i = ini; i < word.Length; i++) {
+          int count = 0;
+          var r = word[i];
+
+          for (int i2 = 0; i2 < word.Length; i2++) {
+
+            var r2 = word[i2];
+
+            if (word[i] == word[i2] && "aeiou".Contains(word[i2])) {
+              
+
+              int o = 0;
+
+              for (int i3 = 0; i3 < word.Length; i3++) {
+                if (word[i3] == word[i2])
+                  o++;
+              }
+
+
+              if (o <= 1) {
+                final = word[i];
+                count++;
+              }
+            }
+
+            if (count > 1)
+              final = ' ';
+          }
+        }
+
+      return final;
+    }
+  }
+}
+
